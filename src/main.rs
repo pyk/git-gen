f2638c65ac076f7a108a9e86dcaa3ddc4c9e2a74
pyk <2213646+pyk@users.noreply.github.com>
2025-09-19 14:59:56 +0700

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{
+    ColorChoice,
+    Parser,
+};
+
+#[derive(Parser, Debug)]
+#[command(
+    bin_name = "git commitgen",
+    color = ColorChoice::Auto,
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    long_about = env!("CARGO_PKG_DESCRIPTION"),
+    after_help = "Run `git commitgen help` for more detailed information."
+)]
+struct Args {
+    /// Changes intention
+    #[arg(short, long)]
+    intent: Option<String>,
+}
+
 fn main() {
+    let _args = Args::parse();
+
     println!("Hello, world!");
 }
