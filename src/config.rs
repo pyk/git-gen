68bcf27de8921639efd5ab8bbb6efcfbd706dc2e
pyk <2213646+pyk@users.noreply.github.com>
2025-09-19 18:44:22 +0700

    fix: improves error message

--- src/config.rs
+++ src/config.rs
@@ -25,26 +25,24 @@ fn get_repo_root() -> Result<PathBuf> {
         .output()
         .map_err(|e| {
             error!(
-                "Failed to execute git command",
+                "failed to execute git command",
                 source: e,
-                help: "Ensure that 'git' is installed and in your system's PATH"
+                help: "ensure that 'git' is installed and in your system's PATH"
             )
         })?;
 
     if !output.status.success() {
-        let stderr = String::from_utf8_lossy(&output.stderr).trim().to_string();
         bail!(
-            "Not a git repository (or any of the parent directories)",
-            note: stderr,
-            help: "Please run `git commitgen` from within a git repository"
+            "not a git repository (or any of the parent directories)",
+            help: "please run 'git commitgen' from within a git repository"
         );
     }
 
     let stdout = String::from_utf8(output.stdout).map_err(|e| {
         error!(
-            "Failed to parse git command output",
+            "failed to parse 'git rev-parse --show-toplevel' command output",
             source: e,
-            note: "The output from 'git rev-parse --show-toplevel' was not valid UTF-8"
+            note: "the output from 'git rev-parse --show-toplevel' was not valid UTF-8"
         )
     })?;
 
@@ -57,9 +55,9 @@ pub fn load() -> Result<Config> {
 
     if !config_path.exists() {
         bail!(
-            "Config file 'commitgen.toml' not found at repository root",
+            "config file 'commitgen.toml' not found at repository root",
             help: format!(
-                "Please create a 'commitgen.toml' file in the root of your git repository at '{}'",
+                "please create a 'commitgen.toml' file in the root of your git repository at '{}'",
                 repo_root.display()
             )
         );
@@ -67,17 +65,17 @@ pub fn load() -> Result<Config> {
 
     let content = fs::read_to_string(&config_path).map_err(|e| {
         error!(
-            "Failed to read config file at '{}'", config_path.display(),
+            "failed to read config file at '{}'", config_path.display(),
             source: e,
-            help: "Please check the file's permissions."
+            help: "please check the file's permissions"
         )
     })?;
 
     let config: Config = toml::from_str(&content).map_err(|e| {
         error!(
-            "Failed to parse TOML in '{}'", config_path.display(),
+            "failed to parse and deserialize TOML in '{}'", config_path.display(),
             source: e,
-            help: "Please check that the TOML syntax is valid."
+            help: "please check check for syntax errors or invalid values"
         )
     })?;
 
